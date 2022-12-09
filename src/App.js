7fa2b2f76cc3288a0c4e0a1087aeb5d19b4b6ed2
thisisamal <amalpullan4@gmail.com>
2022-12-09 14:40:36 +0530

    '2;

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import NaveBar from './components/NavBar/NaveBar';
 import './App.css'
+import Banner from './components/Banner/Banner';
+import RowPost from './components/RowPost/RowPost';
 function App() {
   return (
     <div className="App">
      <NaveBar/>
+     <Banner/>
+     <RowPost/>
     </div>
   );
 }
